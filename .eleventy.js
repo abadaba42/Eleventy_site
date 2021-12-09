17f7693195b68affd5d85915c91772ba4c47d5af
Abadaba <z5260632@unsw.edu.au>
2021-12-09 22:53:28 +1100

    added katex and footnote plugins

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,20 @@
 module.exports = function (eleventyConfig) {
 
+    
+    let markdownIt = require("markdown-it");
+    let options = {html: true};
+    let markdownLib = markdownIt(options)
+    
+    //Markdown plugins
+    let markdownItFootnote = require("markdown-it-footnote");
+    let markdownItKatex = require("markdown-it-katex");
+    markdownLib.use(markdownItFootnote);
+    markdownLib.use(markdownItKatex);
+
+
+    eleventyConfig.setLibrary("md", markdownLib);
+    
+
     eleventyConfig.setTemplateFormats([
         "jpg",
         "css", // css is not yet a recognized template extension in Eleventy
